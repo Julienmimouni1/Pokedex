5922bc1e2a4162dc81e6790476946c709c75760c
Julienmimouni1 <julienmimouni1@gmail.com>
2026-02-17 14:31:51 +0100

    Ajout de l'association entre la team et les pokemon avec include + ajout pokemon à une équipe

--- api/controllers/team.controller.js
+++ api/controllers/team.controller.js
@@ -1,7 +1,15 @@
 import { Team } from "../models/team.model.js";
+import { Pokemon } from "../models/pokemon.model.js";
 
 export async function getAllTeam(req, res){
     const teams = await Team.findAll({
+        include: {
+            model: Pokemon,
+            as: 'pokemons',
+            through: {
+                attributes: [] // Permet de ne pas afficher les détails techniques de la table de liaison
+            }
+        }
     })
     res.status(200).json(teams)
 } ;
@@ -12,7 +20,15 @@ export async function createTeam(req, res){
 }
 
 export async function getTeamById(req,res){
-    const team = await Team.findByPk(req.params.id); 
+    const team = await Team.findByPk(req.params.id, {
+        include: {
+            model: Pokemon,
+            as: 'pokemons',
+            through: {
+                attributes: []
+            }
+        }
+    }); 
     if(!team){
         return res.status(404).json({error : 'Team not found'}); 
     }
@@ -37,4 +53,24 @@ export async function updateTeam(req, res){
     // On renvoie l'équipe modifiée au format JSON. UpdatedTeam est un tableau et comme on a filtré par ID, on sait qu'il 
     // y a un seul résultat, donc on prend le premier élément du tableau. 
     res.status(200).json(updatedTeam[0]); 
+}
+
+export async function addPokemonToTeam(req, res){
+    // On récupère les IDs définis dans la route (/:teamId/pokemon/:pokemonId)
+    const { teamId, pokemonId } = req.params;
+
+    const team = await Team.findByPk(teamId);
+    if (!team) {
+        return res.status(404).json({ error: 'Team not found' });
+    }
+
+    const pokemon = await Pokemon.findByPk(pokemonId);
+    if (!pokemon) {
+        return res.status(404).json({ error: 'Pokemon not found' });
+    }
+// cela permet d'executer une requête SQL INSERT dans ma table de liaison. Exemple: Si team a l'ID 1 (Team Rocket) et pokemon a l'ID 25 (Pikachu),
+// Sequelize va executer quelque chose comme ça : INSERT INTO "team_pokemon" ("team_id", "pokemon_id") VALUES (1, 25);
+// en fait, ca modifie pas la table Team ni la table Pokemon mais ca crée uniquement le lien entre les deux tables. 
+    await team.addPokemon(pokemon);
+    res.status(200).json(team);
 }
--- api/routes/team.routes.js
+++ api/routes/team.routes.js
@@ -1,6 +1,6 @@
 import express from "express"; 
 
-import { getAllTeam, createTeam, getTeamById, updateTeam} from "../controllers/team.controller.js";
+import { getAllTeam, createTeam, getTeamById, updateTeam, addPokemonToTeam} from "../controllers/team.controller.js";
 import { validateTeamCreation, validateTeamUpdate } from "../middlewares/team.middleware.js";
 import { validateID } from "../middlewares/common.middleware.js";
 
@@ -10,6 +10,7 @@ router.get('/', getAllTeam);
 router.get('/:id', validateID, getTeamById)
 
 router.post('/', validateTeamCreation, createTeam)
+router.post('/:teamId/pokemon/:pokemonId', addPokemonToTeam)
 
 router.patch('/:id', validateID, validateTeamUpdate, updateTeam)
 
